0187e0c2daffbea9b638974ca9dc76ebeeb74566
Scott Maxwell <scott@scotteh.me>
2024-06-21 16:53:01 +0100

    Update

--- build.ps1
+++ build.ps1
@@ -6,8 +6,6 @@ cd build
 
 cmake `
     -G "Visual Studio 16 2019" -A x64 `
-    # -DCMAKE_PREFIX_PATH=C:\Miniconda\envs\pygmo `
-    # -DCMAKE_INSTALL_PREFIX=C:\Miniconda\envs\pygmo `
     -DBoost_NO_BOOST_CMAKE=ON `
     -DPAGMO_WITH_EIGEN3=ON `
     -DPAGMO_WITH_IPOPT=ON `
