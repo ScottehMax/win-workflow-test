6406bfafe878766fc2baeb3177dccbb5c20b8104
Scott Maxwell <scott@scotteh.me>
2024-06-22 09:57:16 +0100

    update

--- extract-eigen.ps1
+++ extract-eigen.ps1
@@ -1,8 +1,8 @@
 cd $Env:GITHUB_WORKSPACE
 wget https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz -OutFile eigen-3.4.0.tar.gz
-tar -xvzf eigen-3.4.0.tar.gz eigen
+tar -xvzf eigen-3.4.0.tar.gz eigen-3.4.0
 
-cd eigen
+cd eigen-3.4.0
 mkdir build
 cd build
 
