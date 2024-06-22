399fb45bebd10601ec68486e4abf45b7a65a47fe
Scott Maxwell <scott@scotteh.me>
2024-06-22 09:37:14 +0100

    update

--- extract-eigen.ps1
+++ extract-eigen.ps1
@@ -1,3 +1,6 @@
-cd C:\
+cd $Env:GITHUB_WORKSPACE
 wget https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz -OutFile eigen-3.4.0.tar.gz
 tar -xvzf eigen-3.4.0.tar.gz eigen-3.4.0
+
+# move the eigen to C: (powershell)
+mv eigen-3.4.0 C:\eigen
