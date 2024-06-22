18766d7f6b419d11f9cd2992c12ea4c644a43908
Scott Maxwell <scott@scotteh.me>
2024-06-22 09:46:33 +0100

    update

--- extract-eigen.ps1
+++ extract-eigen.ps1
@@ -1,6 +1,16 @@
 cd $Env:GITHUB_WORKSPACE
 wget https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz -OutFile eigen-3.4.0.tar.gz
-tar -xvzf eigen-3.4.0.tar.gz eigen-3.4.0
+tar -xvzf eigen-3.4.0.tar.gz eigen
 
-# move the eigen to C: (powershell)
-mv eigen-3.4.0 C:\eigen
+cd eigen
+mkdir build
+cd build
+
+cmake `
+    -G "Visual Studio 17 2022" -A x64 `
+    -DCMAKE_PREFIX_PATH=$prefix `
+    -DCMAKE_INSTALL_PREFIX=$prefix `
+    -DCMAKE_CXX_FLAGS="/MP" `
+    -DCMAKE_BUILD_TYPE=Release `
+
+cmake --build . --config Release --target INSTALL
